0bc202ef00420ba3d243c2779a0a4f508cd60646
shubhhjhaa <shubhhjhaa@gmail.com>
2025-12-01 23:25:42 +0530

    Add files via upload

---
+++ practice_9_q2.java
@@ -0,0 +1,11 @@
+class Cylinder{
+    public Cylinder(int height, int radius){
+        System.err.printf("Height : %d Radius : %d ",height,radius);
+    }
+
+}
+public class practice_9_q2 {
+    public static void main(String[]args){
+   Cylinder c1=new Cylinder(10,20);
+    }
+}
